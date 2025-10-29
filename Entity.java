07131eaf234c33b4d54c7f7b565cefdb8e7c72ca
Itsc <itschokky@gmail.com>
2025-10-29 20:09:44 +0700

    update abs

--- Entity.java
+++ Entity.java
@@ -1,5 +1,5 @@
 import java.awt.image.*;;
-public class Entity {
+public abstract class Entity {
     public BufferedImage up1,up2,down1,down2,left1,left2,right1,right2,downe1;
     public String direction;
     public int spriteCounter = 0;
